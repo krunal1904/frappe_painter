67be26a4c1cd8224b59f4eb23a17c8fc24994b9d
krunal prajapati <107530319+krunal1904@users.noreply.github.com>
2026-03-08 13:28:14 +0530

    Delete frappe_painter/overrides directory

--- frappe_painter/overrides/theme.py
+++
@@ -1,7 +0,0 @@
-import frappe
-
-@frappe.whitelist()
-def switch_theme(theme):
-    if theme in ["light", "dark", "automatic", "frappe_painter"]:
-        frappe.db.set_value("User", frappe.session.user, "desk_theme", theme)
-    return theme
