b3b4c6602bc0d04dcf4bb34826fabb16dfed3171
krunal1904 <krunalprajapati1904@gmail.com>
2026-03-08 13:06:49 +0530

    first new version commit

---
+++ frappe_painter/hooks.py
@@ -0,0 +1,9 @@
+app_name = "frappe_painter"
+app_title = "Frappe Painter"
+app_publisher = "TSE"
+app_description = "Comprehensive UI/UX Theme Customizer for Frappe/ERPNext"
+app_version = "5.0.0"
+app_license = "MIT"
+
+app_include_css = ["/assets/frappe_painter/css/painter.css"]
+app_include_js  = ["/assets/frappe_painter/js/painter.js"]
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+setup(
+    name="frappe_painter",
+    version="5.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+)
